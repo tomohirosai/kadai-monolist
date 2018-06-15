a556fe2b9d45eae559bb6afa1f3b16612e957df1
tomohiro.sai <tomohiro.sai@rakuten.com>
2018-06-15 06:20:31 +0000

    ranking

---
+++ app/Http/Controllers/RankingController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+
+use App\Item;
+
+class RankingController extends Controller
+{
+    public function want()
+    {
+        $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->where('type', 'want')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('count', 'DESC')->take(10)->get();
+        $rank_name = 'Want';
+        
+        return view('ranking.want', [
+            'items' => $items,
+            'rank_name' => $rank_name,
+        ]);
+    }
+    
+    public function have()
+    {
+        $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->where('type', 'have')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('count', 'DESC')->take(10)->get();
+        $rank_name = 'Have';
+
+        return view('ranking.have', [
+            'items' => $items,
+            'rank_name' => $rank_name,
+        ]);
+    }
+}
